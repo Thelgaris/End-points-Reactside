b780387a4d1b1f1c3cd5805bdace64a87d243797
Thelgaris <alex.gabarron84@gmail.com>
2022-05-02 19:56:37 +0000

    commit1

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,27 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 import "../../styles/home.css";
 
-export const Home = () => (
+
+
+export const Home = () => {
+	const [newUser, setNewUser] = useState("")
+
+	const createUser = async () => {
+		const response = await fetch("https://3000-thelgaris-flaskresthell-mn6bjxci1db.ws-eu43.gitpod.io/user", {
+			method: 'POST',
+			body: JSON.stringify({"name": newUser}),
+			headers: {'content-type': 'application/json'},
+		})
+		const data = await response.json()
+		console.log("@@@@@@@@@@@", data)
+	}
+
+
+	return(
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+		<input onChange={(e)=> {setNewUser(e.target.value)}}></input>
+		<button onClick={()=>{createUser()}}>Create User</button>
+	
+	</div>)
+
+}
